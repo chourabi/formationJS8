fe491cf603225dedc0c0254734b99b5eb130105b
chourabi taher <tchourabi@gmail.com>
2020-11-07 16:30:23 +0100

    js

---
+++ 11072020/assets/js/main.js
@@ -0,0 +1,43 @@
+console.log("js is working successfully");
+/*
+document.getElementById("mypara").innerHTML= "hello world";
+document.getElementsByClassName("mypara") 
+*/
+
+
+var arr = [
+    {
+        date: new Date()
+    },
+    {
+        date: new Date()
+    },
+    {
+        date: new Date()
+    },
+    {
+        date: new Date()
+    },
+    {
+        date: new Date()
+    },
+    {
+        date: new Date()
+    },
+    
+];
+
+arr.sort();
+
+
+console.log(arr);
+
+
+
+
+
+
+
+
+
+
